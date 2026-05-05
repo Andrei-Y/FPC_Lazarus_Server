7fc0941f13709e0a1a1e2b0edfd8b50cbc129af3
Andrei-Y <49129973+Andrei-Y@users.noreply.github.com>
2026-05-05 21:32:56 +0300

    Add files via upload

--- unit_worker.pas
+++ unit_worker.pas
@@ -8,6 +8,7 @@ uses
 type
   { Переносим тип сюда, ПЕРЕД описанием класса }
   TLogEvent = procedure(const AMsg: string) of object;
+  THTMLEvent = procedure(const AHtml: string) of object; // Добавь это
 
   TWorkerTask = (wtIdle, wtModeration, wtForecast, wtVacuum);
 
@@ -18,14 +19,17 @@ type
     FDB: TDatabaseModule;
     FOnLog: TLogEvent; // Теперь компилятор знает, что это такое
     FMsgForLog: string;
+    FOnHtml: THTMLEvent; // Ссылка на вывод HTML
+    FHtmlBuffer: string; // Временный буфер
     // ... остальное
 
     procedure DoLog(const AMsg: string);
     procedure SyncLog;  // Метод для синхронизации
+    procedure SyncHtml;
   protected
     procedure Execute; override;
   public
-    constructor Create(ADB: TDatabaseModule; ALogEvent: TLogEvent; CreateSuspended: boolean);
+    constructor Create(ADB: TDatabaseModule; ALogEv: TLogEvent; AHtmlEv: THTMLEvent; CreateSuspended: boolean);
     procedure AddMessageTask(AParentID: Integer; AContent: string);
     procedure ExposeSystem(AStartID: Integer);
   end;
@@ -37,13 +41,20 @@ type
 
 implementation
 
-constructor TServerWorker.Create(ADB: TDatabaseModule; ALogEvent: TLogEvent; CreateSuspended: boolean);
+constructor TServerWorker.Create(ADB: TDatabaseModule; ALogEv: TLogEvent; AHtmlEv: THTMLEvent; CreateSuspended: boolean);
 begin
   inherited Create(CreateSuspended);
   FDB := ADB;
-  FOnLog := ALogEvent; // Не забудь присвоить событие!
+  FOnLog := ALogEv;
+  FOnHtml := AHtmlEv;
   FreeOnTerminate := True;
 end;
+
+procedure TServerWorker.SyncHtml;
+begin
+  if Assigned(FOnHtml) then FOnHtml(FHtmlBuffer);
+end;
+
 
 procedure TServerWorker.AddMessageTask(AParentID: Integer; AContent: string);
 var
@@ -87,13 +98,15 @@ end;
 
 procedure TServerWorker.ExposeSystem(AStartID: Integer);
 var
-  CurrentID: Integer;
-  Chrono: string;
-  NodeB, NodeT: Integer;
-  StrList, TailStack: TStringList;
+  CurrentID, NodeB, NodeT, i, j, VisualLevel: Integer;
+  Chrono, NodeContent, S_Open, S_Close, HTML_Row: string;
+  StrList, TailStack, HTML_Acc: TStringList;
 begin
-  DoLog('--- СТАРТ ФОРМИРОВАНИЯ СТРУКТУРЫ ---');
+  HTML_Acc := TStringList.Create;
+  // Темная тема: фон #1e1e1e, текст #d4d4d4
+  HTML_Acc.Add('<html><body style="font-family:sans-serif; background:#1e1e1e; color:#d4d4d4; padding:15px;">');
 
+  DoLog('--- СТАРТ ФОРМИРОВАНИЯ СТРУКТУРЫ ---');
   CurrentID := AStartID;
   StrList := TStringList.Create;
   TailStack := TStringList.Create;
@@ -111,12 +124,7 @@ begin
       NodeB := StrToIntDef(StrList[1], 0);
       NodeT := StrToIntDef(StrList[2], 0);
 
-      // --- ПЕРЕНЕСЛИ СЮДА: ФИКСИРУЕМ УЗЕЛ СРАЗУ ---
-      // Мы выводим его только если еще не "ныряли" из него (его нет в стеке)
-      if TailStack.IndexOf(IntToStr(CurrentID)) = -1 then
-         DoLog('ВЫДЕРНУТ УЗЕЛ: ' + IntToStr(CurrentID));
-
-      // ПРОВЕРКА НА НЫРОК:
+      // --- ШАГ 1: НЫРОК ---
       if (NodeT <> 0) and (TailStack.IndexOf(IntToStr(CurrentID)) = -1) then
       begin
         DoLog('>>> НЫРОК В ВЕТКУ (из ' + IntToStr(CurrentID) + ')');
@@ -125,30 +133,64 @@ begin
         Continue;
       end;
 
-      // ПРОВЕРКА НА ВСПЛЫТИЕ:
+      // --- ШАГ 2: ОПРЕДЕЛЯЕМ ВИЗУАЛЬНЫЙ УРОВЕНЬ ---
+      // Если узел в стеке — значит это РОДИТЕЛЬ, из которого мы вынырнули.
+      // Чтобы дети были ПРАВЕЕ него, его уровень должен быть меньше.
+      if TailStack.IndexOf(IntToStr(CurrentID)) <> -1 then
+        VisualLevel := TailStack.Count - 1
+      else
+        VisualLevel := TailStack.Count;
+
+      // Защита от отрицательного уровня
+      if VisualLevel < 0 then VisualLevel := 0;
+
+      // --- ШАГ 3: ФИКСАЦИЯ И ОТРИСОВКА ---
+      NodeContent := FDB.GetNodeContent(CurrentID);
+      DoLog('ВЫДЕРНУТ УЗЕЛ: ' + IntToStr(CurrentID));
+
+      S_Open := ''; S_Close := '';
+      for j := 1 to VisualLevel do
+      begin
+        // Добавляем valign="top" и убираем любые зазоры в ячейке линии
+        S_Open := S_Open +
+          '<table border="0" cellspacing="0" cellpadding="0"><tr>' +
+          '<td width="20" valign="top"></td>' + // Пустой отступ
+          // Линия: убираем лишние стили, оставляем только цвет и выравнивание
+          '<td width="2" bgcolor="#4A90E2" valign="top" style="line-height:1px; font-size:1px;">&nbsp;</td>' +
+          '<td width="10" valign="top"></td>' + // Просвет
+          '<td valign="top">'; // Контентная часть
+
+        S_Close := '</td></tr></table>' + S_Close;
+      end;
+
+      // Само сообщение
+      HTML_Row := S_Open +
+                  '<table border="0" cellpadding="8" cellspacing="0" width="100%" bgcolor="#2d2d2d" style="margin-bottom:2px;">' +
+                  '<tr><td style="border: 1px solid #3e3e3e;" valign="top">' +
+                  '<font color="#6a9955" size="1">ID: ' + IntToStr(CurrentID) + '</font><br>' +
+                  '<font color="#d4d4d4">' + NodeContent + '</font>' +
+                  '</td></tr></table>' +
+                  S_Close;
+
+      HTML_Acc.Add(HTML_Row);
+
+      // --- ШАГ 4: ВСПЛЫТИЕ ---
       if TailStack.IndexOf(IntToStr(CurrentID)) <> -1 then
       begin
          TailStack.Delete(TailStack.IndexOf(IntToStr(CurrentID)));
          DoLog('<<< ВСПЛЫТИЕ ИЗ ВЕТКИ (возврат в ' + IntToStr(CurrentID) + ')');
       end;
 
-      CurrentID := NodeB;
-
       if (CurrentID = AStartID) and (TailStack.Count = 0) then Break;
+      CurrentID := NodeB;
     end;
 
-    // Корень выводим отдельно в конце, если цикл на нем завершился
-    if (AStartID <> 0) and (CurrentID = AStartID) then
-       DoLog('ВЫДЕРНУТ УЗЕЛ (КОРЕНЬ): ' + IntToStr(AStartID));
-
-
-
-
-
-
-    if (AStartID <> 0) then DoLog('ВЫДЕРНУТ УЗЕЛ (КОРЕНЬ): ' + IntToStr(AStartID));
+    HTML_Acc.Add('</body></html>');
+    FHtmlBuffer := HTML_Acc.Text;
+    Synchronize(@SyncHtml);
 
   finally
+    HTML_Acc.Free;
     StrList.Free;
     TailStack.Free;
   end;
@@ -156,72 +198,6 @@ begin
 end;
 
 
-
-
-
-
-
-
-
-
-//procedure TServerWorker.ExposeSystem(AStartID: Integer);
-//var
-//  CurrentID, LastID: Integer;
-//  Chrono: string;
-//  NodeB, NodeT: Integer;
-//  StrList: TStringList;
-//begin
-//  // МАЯК 1: Вход в процедуру
-//  FDB.ExecSQL('INSERT INTO nodes (content, chronology) VALUES (''Воркер: Начал обход системы ' + IntToStr(AStartID) + ''', ''0.0.0.'')');
-//
-//  CurrentID := AStartID;
-//  LastID := 0;
-//  StrList := TStringList.Create;
-//  try
-//    StrList.Delimiter := '.';
-//    StrList.StrictDelimiter := True;
-//
-//    Chrono := FDB.GetNodeChrono(AStartID);
-//
-//    // МАЯК 2: Что прочитали из головы
-//    FDB.ExecSQL('INSERT INTO nodes (content, chronology) VALUES (''Воркер: Голова ' + IntToStr(AStartID) + ' имеет хроно ' + Chrono + ''', ''0.0.0.'')');
-//
-//    StrList.DelimitedText := Chrono;
-//    if StrList.Count > 2 then
-//      CurrentID := StrToIntDef(StrList[2], 0) // Хвост
-//    else
-//      Exit;
-//
-//    while (CurrentID <> 0) and (CurrentID <> AStartID) do
-//    begin
-//      Chrono := FDB.GetNodeChrono(CurrentID);
-//      StrList.DelimitedText := Chrono;
-//      if StrList.Count < 3 then Break;
-//
-//      NodeB := StrToIntDef(StrList[1], 0);
-//      NodeT := StrToIntDef(StrList[2], 0);
-//
-//      // МАЯК 3: Лог каждого шага в цикле
-//      FDB.ExecSQL('INSERT INTO nodes (content, chronology) VALUES (''Воркер: Читаю узел ' + IntToStr(CurrentID) + ' (B:' + IntToStr(NodeB) + ' T:' + IntToStr(NodeT) + ')'', ''0.0.0.'')');
-//
-//      if (NodeT <> 0) and (NodeT <> LastID) then
-//      begin
-//        LastID := CurrentID;
-//        CurrentID := NodeT;
-//      end
-//      else
-//      begin
-//        LastID := CurrentID;
-//        CurrentID := NodeB;
-//      end;
-//    end;
-//  finally
-//    StrList.Free;
-//  end;
-//
-//  // МАЯК 4: Конец
-//  FDB.ExecSQL('INSERT INTO nodes (content, chronology) VALUES (''Воркер: Обход завершен успешно'', ''0.0.0.'')');
-//end;
 
 
 
